a20fe5e640300ec445da4959d5522d54bde0bde4
TimothyJounier <timothyjounier@gmail.com>
2023-05-10 10:15:20 +0200

    Build Onboarding Screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -72,9 +72,44 @@ struct OnboardingView: View {
                         .fill(Color.white.opacity(0.2))
                         .padding(8)
                     // 2. CALL TO ACTION
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
                     // 3. CAPSULE
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
                     // 4. CIRCLE
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    }
                 } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
             } //: VSTACK
         } //: ZSTACK
     }
